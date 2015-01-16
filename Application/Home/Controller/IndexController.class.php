d88743121935f93d30b1cd4ef2f7abfbb3df95e5
mangle <gotaotao123@163.com>
2015-01-16 15:58:07 +0800

    adfasdfs

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,6 +4,7 @@ use Think\Controller;
 class IndexController extends Controller {
     public function index()
 	{
+        echo "<meta charset='utf-8'>";
        	$res = M('words')->limit('10,2')->select();	
        	dump($res);		
     }
