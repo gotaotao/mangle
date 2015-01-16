c5fe89f3be26f705a5a377d9d3e06162b072f5b3
mangle <gotaotao123@163.com>
2015-01-16 16:35:41 +0800

    sdfasdf
    asdfasdfasdf

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -2,10 +2,10 @@
 return array(
 	//'配置项'=>'配置值'
 	'DB_TYPE' => 'mysql',
-	'DB_HOST' => '58.83.218.139',
-	'DB_NAME' => 'baikev2',
+	'DB_HOST' => '123.56.108.167',
+	'DB_NAME' => 'baike',
 	'DB_USER' => 'root',
-	'DB_PWD' => 'root',
+	'DB_PWD' => '880829',
 	'DB_PORT' => '3306',
 	'DB_PREFIX' => 'jk_',
 	'URL_CASE_INSENSITIVE' =>true，
